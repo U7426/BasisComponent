1fcd969862c725a21f718324b41743a4d68c120f
fenglongfei <u7426fenglongfei@163.com>
2020-08-03 18:23:09 +0800

    添加部分扩展

--- BasisComponent/Classes/UITableView+Extension.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UITableView+Extension.swift
-//  BasisComponent
-//
-//  Created by 冯龙飞 on 2020/8/3.
-//
-
-import Foundation
--- Example/BasisComponent/ViewController.swift
+++ Example/BasisComponent/ViewController.swift
@@ -7,11 +7,11 @@
 //
 
 import UIKit
+import BasisComponent
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
